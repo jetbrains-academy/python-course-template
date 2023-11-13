ef04a71cecf5da64ec751742a266067cab2f0327
Maria Tigina <maria.tigina@jetbrains.com>
2023-11-13 17:39:44 +0100

    Fix tests and changed logo

--- course_section/course_lesson/programming_task/task.md
+++ course_section/course_lesson/programming_task/task.md
@@ -1,5 +1,5 @@
-This task asks the user to implement something and runs tests from the [Tests.kt](./test/Tests.kt) file.
-You need to add _placeholders_ into the places where the user needs to implement something (see [task-info.yaml](./task-info.yaml)).
+This task asks the user to implement something and runs tests from the [test.py](tests/test.py) file.
+You need to add _placeholders_ into the places where the user needs to implement something (see [task-info.yaml](task-info.yaml)).
 
 This task also demonstrates how you can use the [kotlin test framework](https://github.com/jetbrains-academy/kotlin-test-framework) 
 to test functions or classes without using the correct implementation in the tests. 
--- course_section/course_lesson/programming_task/tests/test.py
+++ course_section/course_lesson/programming_task/tests/test.py
@@ -1,20 +1,23 @@
 import os
+import unittest
 from unittest.mock import patch
-
-import pytest
 
 from course_section.course_lesson.programming_task.invisible_main import say_hello
 from course_section.course_lesson.programming_task.main import invoke_say_hello
 
 
-def correct_invoke_say_bye(how_many_times: int):
-    return os.linesep.join([say_hello() for _ in range(how_many_times)])
+class TestAddFunction(unittest.TestCase):
+    test_cases = [2, 4, 20, 1900, 400, 2004, 2000, 2001]
 
+    @staticmethod
+    def correct_invoke_say_bye(how_many_times: int):
+        return os.linesep.join([say_hello() for _ in range(how_many_times)])
 
-@patch('builtins.print')
-@pytest.mark.parametrize("how_many_times", [2, 4, 20, 1900, 400, 2004, 2000, 2001])
-def test_invoke_say_buy(mock_print, how_many_times: int):
-    correct_solution = correct_invoke_say_bye(how_many_times)
-    invoke_say_hello(how_many_times)
-    mock_print.assert_called_once_with(correct_solution)
-    mock_print.reset_mock()
+    @patch('builtins.print')
+    def test_parametrized(self, mock_print):
+        for how_many_times in self.test_cases:
+            with self.subTest(how_many_times):
+                correct_solution = self.correct_invoke_say_bye(how_many_times)
+                invoke_say_hello(how_many_times)
+                mock_print.assert_called_once_with(correct_solution)
+                mock_print.reset_mock()
